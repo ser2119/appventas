23f5bbb6a89a4cba5f2d21c9f1ebdd1a7dfbdaea
Sergio Garcia Urbano <sgarciau@outlook.com>
2019-09-20 14:07:10 -0500

    Stored Procedures
    Cambios SP

---
+++ StoredeProcedures/sp_editar_categoria.sql
@@ -0,0 +1,12 @@
+﻿create procedure sp_editar_categoria
+@v_id int,
+@v_nombre VARCHAR(50),
+@v_descripcion VARCHAR(2)
+as
+begin
+	update  t01_categoria
+	set f01_nombre = @v_nombre, 
+	f01_descripcion = @v_descripcion 
+	where f01_id = @v_id
+end
+go
---
+++ StoredeProcedures/sp_eliminar_categoria.sql
@@ -0,0 +1,8 @@
+﻿create procedure sp_eliminar_categoria
+@v_id int
+as
+begin
+	delete  t01_categoria
+	where f01_id = @v_id
+end
+go
