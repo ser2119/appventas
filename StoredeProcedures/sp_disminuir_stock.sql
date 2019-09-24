2ee79c6bad5fa762bfe31aea4a758636094e1c6a
Sergio Garcia Urbano <sgarciau@outlook.com>
2019-09-24 15:14:39 -0500

    sp
    sp

---
+++ StoredeProcedures/sp_disminuir_stock.sql
@@ -0,0 +1,13 @@
+﻿alter procedure sp_disminuir_stock
+@p_id int,
+@p_cantidad int
+
+as
+
+begin
+
+update t08_detalle_ingreso
+set f08_stock_actual = f08_stock_actual - @p_cantidad
+where f08_id = @p_id
+ 
+end
---
+++ StoredeProcedures/sp_editar_articulo.sql
@@ -0,0 +1,21 @@
+﻿alter procedure sp_editar_articulo
+@p_id int,
+@p_codigo VARCHAR(50),
+@p_nombre VARCHAR(50),
+@p_descripcion VARCHAR(1024),
+@p_image image,
+@p_id_categoria int,
+@p_id_presentacion int
+
+as
+begin
+update t06_articulo
+set f06_codigo = @p_codigo,
+	f06_descripcion = @p_descripcion,
+	f06_id_categoria = @p_id_categoria,
+	f06_id_presentacion = @p_id_presentacion,
+	f06_imagen = @p_image,
+	f06_nombre = @p_nombre
+where f06_id = @p_id
+end
+
